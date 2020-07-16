79248087755780db1ddbdb542464ec7bf31214af
Dominick Rivard <643002+drivard@users.noreply.github.com>
2020-07-16 17:49:57 -0400

    Added support for Wireguard.

--- docker-install.sh
+++ docker-install.sh
@@ -1,7 +1,7 @@
 set -ex
 
 apt-get update -q
-apt-get install -y locales iptables wget gnupg2
+apt-get install -q -y locales iptables wget gnupg2
 
 if [ "${MONGODB_VERSION}" != "no" ]; then
     echo 'deb https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse' > /etc/apt/sources.list.d/mongodb-org-4.0.list
@@ -9,10 +9,12 @@ if [ "${MONGODB_VERSION}" != "no" ]; then
 fi
 
 echo 'deb https://repo.pritunl.com/stable/apt bionic main' > /etc/apt/sources.list.d/pritunl.list
+echo 'deb http://ppa.launchpad.net/wireguard/wireguard/ubuntu bionic main'  > /etc/apt/sources.list.d/wireguard.list
 #echo "deb http://build.openvpn.net/debian/openvpn/stable bionic main" > /etc/apt/sources.list.d/openvpn-aptrepo.list
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-#apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 8E6DA8B4E158C569
+# Wireguard
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AE33835F504A1A25
 
 locale-gen en_US en_US.UTF-8
 dpkg-reconfigure locales
@@ -23,6 +25,8 @@ apt-get dist-upgrade -y -q
 # install pritunl
 apt-get update -q
 apt-get install -y -q pritunl
+# install wireguard
+DEBIAN_FRONTEND=noninteractive apt-get install -y -q wireguard
 
 if [ "${MONGODB_VERSION}" != "no" ]; then
     apt-get -y install mongodb-org=${MONGODB_VERSION};
